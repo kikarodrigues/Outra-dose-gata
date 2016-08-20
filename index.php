6cbf3700aedd6f8418d6a0579653f86e5d2ec811
kika <outradosegata@gmail.com>
2016-08-20 18:00:33 -0300

    oi

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-hello world! asdd
-add 
+hello world!
+O blog está em manutenção!! Em breve voltaremos :)
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>o php so escreve texto </title>
+  </head>
+  <body>
+
+  </body>
+</html>
